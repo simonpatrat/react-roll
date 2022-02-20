8ebaa5df37eead6275e133f07ff280139d3f2e51
simonpatrat <simon.patrat@gmail.com>
2022-02-21 00:52:23 +0100

    add rollup-plugin-typescript2 instead of rollup-plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
 
