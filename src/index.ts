ae174254de5399730615236c54ae982dc837dc12
simonpatrat <simon.patrat@gmail.com>
2022-02-20 21:46:06 +0100

    Update config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export { Carousel, Slide } from "./components";
-
-export type { CarouselProps } from "./components/Carousel/Carousel";
-export type { SlideProps } from "./components/Slide/Slide";
-export type { SlideItem } from "./lib/useSlides";
+export * from "./components";
