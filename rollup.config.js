3c391336c9a95610aea0bb3d078ac5a74ae10469
simonpatrat <simon.patrat@gmail.com>
2022-02-21 13:19:46 +0100

    scss files in dist

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
 // import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
@@ -30,6 +31,15 @@ export default [
         useTsconfigDeclarationDir: true,
       }),
       postcss(),
+      copy({
+        targets: [
+          {
+            src: "src/components/Carousel/carousel.scss",
+            dest: "dist",
+            rename: "carousel.scss",
+          },
+        ],
+      }),
     ],
   },
   // {
